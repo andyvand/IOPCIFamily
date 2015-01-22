4a85808a00d9e41ff359c9a6b1ca1cb1f94fdfa8
Joris Schepers <joris@imac-van-joris.home>
2015-01-22 20:21:16 +0100

    Initial setup

---
+++ build/IOPCIFamily.build/Deployment/IOPCIFamily.build/DerivedSources/IOPCIFamily_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char IOPCIFamilyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:IOPCIFamily  PROJECT:IOPCIFamily-2.5" "\n"; const double IOPCIFamilyVersionNumber __attribute__ ((used)) = (double)2.5;
